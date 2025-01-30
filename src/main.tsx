7a8e3cacdd7f1dfd8b3ae9956c3a4d820aad532a
DanielRustrum <danieltrustrum@gmail.com>
2025-01-30 10:30:30 -0700

    Changes; build to itch

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import './index.scss'
 
 import { Menu } from './pages/Menu'
@@ -12,13 +12,27 @@ import { Explore } from './pages/Explore';
 import { Tutorial } from './pages/Tutorial';
 import { setupBackgroundMusic } from './hooks/useBackgroundMusic';
 
+const NotFound = () => {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    navigate("/")
+  }, [])
+
+
+  return <p>Loading Home</p>
+}
+
 const Global = () => {
   const MusicWrapper = setupBackgroundMusic()
   
+  const router_props = {
+    basename: import.meta.env.BASE_URL === "/"? undefined: import.meta.env.BASE_URL
+  }
 
   return (
     <MusicWrapper>
-      <BrowserRouter basename="/GameJam">
+      <BrowserRouter {...router_props}>
         <Routes>
           <Route path="/" element={<Menu />} />
           <Route path="/field" element={<Field />} />
@@ -27,6 +41,7 @@ const Global = () => {
           <Route path="/end-game" element={<End />} />
           <Route path="/town" element={<Town />} />
           <Route path="/tutorial" element={<Tutorial />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </MusicWrapper>
