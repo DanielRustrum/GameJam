5117c695f71c831c3bef8fa5192e5e863e9ca574
DanielRustrum <danieltrustrum@gmail.com>
2025-01-30 10:46:40 -0700

    Changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import zipPack from "vite-plugin-zip-pack";
 
+const repo_sub_path = "/GameJam"
+let base = "/GameJam"
+let plugins;
 
+console.log(process.env.APP_ENV)
+if(process.env.APP_ENV === "github") {
+  base = repo_sub_path
+  plugins = [react()]
+} else {
+  plugins = [react(), zipPack()]
+}
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/GameJam/"
+  plugins: plugins,
+  base: base
 })
