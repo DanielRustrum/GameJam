9c63d1dc8b7c3e74a9460df54ded78445a898cd5
DanielRustrum <danieltrustrum@gmail.com>
2025-06-22 19:00:04 -0700

    Last Credit Change

--- assets/credits.md
+++ assets/credits.md
@@ -1,11 +1,13 @@
 This is the file for game creditation
 
+<space></space>
+
 ---
 
-<div class="grid grid-cols-3 grid-rows-2 gap-4 m-20">
-    <contributor name="Jane Doe" role="Artist" avatar="/avatars/jane.png">This is a description</contributor>
-    <contributor name="Jane Doe1" role="Artist" avatar="/avatars/jane.png"></contributor>
-    <contributor name="Jane Doe2" role="Artist" avatar="/avatars/jane.png"></contributor>
-    <contributor name="Jane Doe3" role="Artist" avatar="/avatars/jane.png"></contributor>
-    <contributor name="Jane Doe3" role="Artist" avatar="/avatars/jane.png"></contributor>
+<div class="flex flex-wrap m-20">
+    <contributor name="Jane Doe" role="Artist" avatar="jane">This is a description</contributor>
+    <contributor name="Jane Doe1" role="Artist" avatar="jane"></contributor>
+    <contributor name="Jane Doe2" role="Artist" avatar="jane"></contributor>
+    <contributor name="Jane Doe3" role="Artist" avatar="jane"></contributor>
+    <contributor name="Jane Doe3" role="Artist" avatar="jane"></contributor>
 </div>
--- src/panels/Credits.tsx
+++ src/panels/Credits.tsx
@@ -4,17 +4,22 @@ import { usePanelNavigation } from "@engine/panel"
 import { Button } from '@/components/UI/button';
 import { ReactNode } from 'react';
 import '@panels/credits.scss'
+import { ScrollArea } from "@ui/scroll-area"
 
 import credit_markdown from '@assets/credits.md?raw'
 
+const AVATARS:{[key: string]: string} = {
+    "": ""
+}
+
 function Contributor({ name, role, avatar, children }: { name: string; role: string; avatar: string, children: ReactNode }) {
     return (
-        <div className="contributor-card m-auto">
-            <img src={avatar} alt={name} width={64} height={64} />
+        <div className="contributor-card m-auto min-w-[30%] justify-center">
+            <img className="m-auto rounded-full" src={AVATARS[avatar]} alt={name} width={64} height={64} />
             <div>
-                <strong>{name}</strong>
-                <div>{role}</div>
-                <div>{children}</div>
+                <p><strong>{name}</strong></p>
+                <p>{role}</p>
+                <p>{children}</p>
             </div>
         </div>
     );
@@ -34,21 +39,24 @@ const components: Partial<Record<string, any>> = {
         const { name, role, avatar } = node.properties ?? {};
         return <Contributor name={name} role={role} avatar={avatar}>{children}</Contributor>;
     },
-    h1: "h2"
+    h1: "h2",
+    space: ({}) => <br />
 };
 
 export const Panel = () => {
     return (
         <div className='flex flex-col mx-5 py-5 gap-10 min-h-dvh justify-between'>
-            <h1 className='text-center text-2xl'>Credits</h1>
-            <div id="markdown-container" className='mx-15 p-5 rounded-md border-0 bg-purple-400 grow h-screen'>
-                <Markdown
-                    rehypePlugins={[rehypeRaw]}
-                    components={components}
-                >
-                    {credit_markdown}
-                </Markdown>
-            </div>
+            <h1 className='text-center text-2xl font-bold'>Credits</h1>
+            <ScrollArea>
+                <div id="markdown-container" className='mx-15 p-5 rounded-md border-0 bg-purple-400 grow h-screen'>
+                    <Markdown
+                        rehypePlugins={[rehypeRaw]}
+                        components={components}
+                    >
+                        {credit_markdown}
+                    </Markdown>
+                </div>
+            </ScrollArea>
             <BackToMenuButton>Back To Main Menu</BackToMenuButton>
         </div>
     )
